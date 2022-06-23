4c93dfb819db01ed49a157e786daa55f84416814
Frank Murphy <88675306+frank-at-suse@users.noreply.github.com>
2022-06-23 10:26:27 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,11 +6,6 @@ terraform {
       source  = "rancher/rancher2"
       version = "1.24.0"
     }
-
-    vsphere   = {
-      source  = "hashicorp/vsphere"
-      version = "2.1.1"
-    }
   } # End of required_providers
 }   # End of terraform
 
@@ -18,11 +13,4 @@ terraform {
     access_key = file( "${path.cwd}/files/.rancher-access-key" )
     api_url    = file( "${path.cwd}/files/.rancher-api-url" )
     secret_key = file( "${path.cwd}/files/.rancher-secret-key" )
-  }
-
-  provider "vsphere" {
-    allow_unverified_ssl = true
-    user                 = var.vsphere_env.user
-    password             = file( "${path.cwd}/files/.vsphere-passwd" )
-    vsphere_server       = var.vsphere_env.server
   }
