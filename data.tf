6370c8b2b19f4bdb9f2c14169cdf79f9bcb790a4
Frank Murphy <frank.murphy@suse.com>
2023-04-12 09:56:49 -0400

    add kube-vip HTTP data sources

--- data.tf
+++ data.tf
@@ -1,3 +1,12 @@
+data "http" "kube_vip_rbac" {
+  url = "https://kube-vip.io/manifests/rbac.yaml"
+}
+
+data "http" "kube_vip_version" {
+  method = "GET"
+  url    = "https://api.github.com/repos/kube-vip/kube-vip/releases/latest"
+}
+
 data "rancher2_cloud_credential" "auth" {
   name = var.rancher_env.cloud_credential
 }
